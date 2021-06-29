3eac83d5503b058816a071e92d562705deb9d647
John-maura <johntemwaura992@gmail.com>
2021-06-29 11:21:32 +0300

    made the topbar together with all its styles

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import "./sidebar.css"
+const Sidebar = () =>{
+    return(
+        <div className="sidebar">
+            <div className="sidebarWrapper">
+                
+            </div>
+        </div>
+    );
+}
+export default Sidebar;
